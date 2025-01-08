f1b102e4b5ae6b6e2603277fc6104914b479a333
gabriel <g.magalhaes.dev@gmail.com>
2025-01-08 20:34:09 -0300

    modal component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AssetComponent } from './components/Asset/asset.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AssetComponent],
+  imports: [RouterOutlet, AssetComponent, ModalComponent],
   templateUrl: "./app.component.html",
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'FacaMinhaColeta';
+
+  public showModal(): void {
+    
+  }
+
 }
